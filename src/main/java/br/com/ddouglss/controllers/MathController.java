07cd7f851fdc3db38294f680e051ac1cac64c1b9
ddouglss <ddouglss1999@gmail.com>
2025-05-17 03:40:06 -0300

    Implementando as Demais Operações Matemáticas

--- src/main/java/br/com/ddouglss/controllers/MathController.java
+++ src/main/java/br/com/ddouglss/controllers/MathController.java
@@ -8,19 +8,76 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
+    //Soma
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo
 
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalAccessException();
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value!");
         return convertToDouble (numberOne) + convertToDouble (numberTwo);
     }
 
+    //subtração
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value!");
+        return convertToDouble (numberOne) - convertToDouble (numberTwo);
+    }
+
+    //multiplicação
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value!");
+        return convertToDouble (numberOne) * convertToDouble (numberTwo);
+    }
+
+    //divisão
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value!");
+        return convertToDouble (numberOne) / convertToDouble (numberTwo);
+    }
+
+    //divisão
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value!");
+        return (convertToDouble (numberOne) + convertToDouble (numberTwo)) / 2;
+    }
+    //raiz quadrada
+    @RequestMapping("/squareRoot/{number}")
+    public Double squareRoot(
+            @PathVariable("number") String number
+
+    ) throws Exception {
+        if (!isNumeric(number)) throw new UnsupportedOperationException("Please set a numeric value!");
+        //Double result = Math.sqrt(convertToDouble(number));
+        //return result;
+        return Math.sqrt(convertToDouble(number));
+    }
+
+
     private Double convertToDouble(String strNumber) throws IllegalAccessException {
 
-        if(strNumber == null || strNumber.isEmpty()) throw new IllegalAccessException();
+        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedOperationException("Please set a numeric value!");
         String number = strNumber.replace(",", "."); //Converte "," por "." : R$ 10,00 USD 10.00
         return Double.parseDouble(number);
     }
---
+++ src/main/java/br/com/ddouglss/exception/hadler/CustomEntityResponseHandler.java
@@ -0,0 +1,36 @@
+package br.com.ddouglss.exception.hadler;
+
+import br.com.ddouglss.exception.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.rmi.server.ExportException;
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ExportException.class)
+    public final ResponseEntity<ExceptionResponse> handleALLExceptions(Exception ex, WebRequest request){
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
