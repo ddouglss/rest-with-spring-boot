84dd585ae694e1fe9930a93704a01f390bc2a191
ddouglss <ddouglss1999@gmail.com>
2025-06-19 20:53:15 -0300

    chore: implemented Service

--- src/main/java/br/com/ddouglss/service/PersonServices.java
+++ src/main/java/br/com/ddouglss/service/PersonServices.java
@@ -1,11 +1,27 @@
 package br.com.ddouglss.service;
 
+import br.com.ddouglss.model.Person;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
+
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Find one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Douglas");
+        person.setLastName("Souza");
+        person.setAddress("Brasília - DF - Brazil");
+        person.setGender("Male");
+        return person;
+    }
 }
