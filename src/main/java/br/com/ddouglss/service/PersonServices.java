fb4987534f6a39e849ae41732d7b2b2b3dadbb98
ddouglss <ddouglss1999@gmail.com>
2025-06-19 21:49:09 -0300

    chore:c ontroller implemented with service simulation using findAll return of people

--- src/main/java/br/com/ddouglss/service/PersonServices.java
+++ src/main/java/br/com/ddouglss/service/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.ddouglss.service;
 import br.com.ddouglss.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,15 @@ public class PersonServices {
 
     private final Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Find one Person!");
 
@@ -22,6 +33,18 @@ public class PersonServices {
         person.setLastName("Souza");
         person.setAddress("Brasília - DF - Brazil");
         person.setGender("Male");
+        return person;
+    }
+
+
+    private Person mockPerson(int i) {
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name "+ i);
+        person.setLastName("Last Name "+ i);
+        person.setAddress("Some Address in Brazil" );
+        person.setGender("Gender " );
         return person;
     }
 }
