3b214b9dd01c8d548f102256d985b4f5ddbdaa20
ddouglss <ddouglss1999@gmail.com>
2025-07-25 01:57:59 -0300

    refactor(person): aplicar DTO e ObjectMapper no service e controller

--- src/main/java/br/com/ddouglss/controllers/PersonController.java
+++ src/main/java/br/com/ddouglss/controllers/PersonController.java
@@ -1,6 +1,6 @@
 package br.com.ddouglss.controllers;
 
-import br.com.ddouglss.model.Person;
+import br.com.ddouglss.data.dto.PersonDTO;
 import br.com.ddouglss.service.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,28 +18,28 @@ public class PersonController {
 
     //@RequestMapping(method = {RequestMethod.GET})
     @GetMapping (produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
+    public List<PersonDTO> findAll(){
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person findById(@PathVariable("id") Long id) {
+    public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person create(@RequestBody Person person){
+    public PersonDTO create(@RequestBody PersonDTO person){
         return service.create(person);
     }
 
     @PutMapping( consumes = MediaType.APPLICATION_JSON_VALUE,
               produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person update(@RequestBody Person person){
+    public PersonDTO update(@RequestBody PersonDTO person){
         return service.update(person);
     }
 
--- src/main/java/br/com/ddouglss/service/PersonServices.java
+++ src/main/java/br/com/ddouglss/service/PersonServices.java
@@ -1,6 +1,9 @@
 package br.com.ddouglss.service;
 
+import br.com.ddouglss.data.dto.PersonDTO;
 import br.com.ddouglss.exception.ResourceNotFoundException;
+import static br.com.ddouglss.mapper.ObjectMapper.parseListObjects;
+import static br.com.ddouglss.mapper.ObjectMapper.parseObject;
 import br.com.ddouglss.model.Person;
 import br.com.ddouglss.repository.PersonRepository;
 import org.slf4j.Logger;
@@ -20,27 +23,29 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         logger.info("Find all People!");
 
-        return repository.findAll();
+        return parseListObjects(repository.findAll(), PersonDTO.class);
     }
 
-    public Person findById(Long id) {
+    public PersonDTO findById(Long id) {
         logger.info("Finding one Person!");
 
-        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return  parseObject(entity, PersonDTO.class);
     }
 
 
-    public Person create(Person person) {
+    public PersonDTO create(PersonDTO person) {
         logger.info("Creating one Person!");
-
-        return repository.save(person);
+        var entity = parseObject(person, Person.class);
+        return parseObject(repository.save(entity),PersonDTO.class);
     }
 
 
-    public Person update(Person person) {
+    public PersonDTO update(PersonDTO person) {
         logger.info("Updating one Person!");
         Person entity = repository.findById(person.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
@@ -50,7 +55,7 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(person);
+        return parseObject(repository.save(entity),PersonDTO.class);
     }
 
     public void delete(Long id) {
